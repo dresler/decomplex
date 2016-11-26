f1485d0b0325336f54131fea044d9f160bec3b20
Robert Dresler <dresler.robert@gmail.com>
2016-11-26 14:14:25 +0100

    Tests for IEnumerable.ForEach() added

--- decomplex.Tests/EnumerableExtensionsTests.cs
+++ decomplex.Tests/EnumerableExtensionsTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -8,6 +9,29 @@ namespace decomplex.Tests
     [TestFixture]
     public class EnumerableExtensionsTests
     {
+        [Test]
+        public void ForEach_ForNullCollection_ShouldThrowArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => (null as IEnumerable<int>).ForEach(x => { }));
+        }
+
+        [Test]
+        public void ForEach_ForNullAction_ShouldThrowArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => Enumerable.Empty<int>().ForEach(null));
+        }
+
+        [Test]
+        public void ForEach_ForCollection_ShouldInvokeEachItem()
+        {
+            var numbers = new[] {4, 7, 1, 5};
+            var iteratedNumbers = new List<int>();
+
+            numbers.ForEach(number => iteratedNumbers.Add(number));
+
+            iteratedNumbers.Should().Equal(numbers);
+        }
+
         [Test]
         public void Yield_WhenInteger_ShouldReturnIEnumerableWithTheInteger()
         {
