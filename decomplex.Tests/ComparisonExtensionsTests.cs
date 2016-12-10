179226832a2b47154679ec4417f9b614b4a18959
Robert Dresler <dresler.robert@gmail.com>
2016-12-10 07:04:44 +0100

    ComparisonExtensions added

---
+++ decomplex.Tests/ComparisonExtensionsTests.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using NUnit.Framework;
+
+namespace decomplex.Tests
+{
+    [TestFixture]
+    public class ComparisonExtensionsTests
+    {
+        [Test]
+        public void IsIn_For1In123_ShouldReturnTrue()
+        {
+            Assert.IsTrue(1.IsIn(1, 2, 3));
+        }
+
+        [Test]
+        public void IsIn_For2In123_ShouldReturnTrue()
+        {
+            Assert.IsTrue(2.IsIn(1, 2, 3));
+        }
+
+        [Test]
+        public void IsIn_For0In123_ShouldReturnFalse()
+        {
+            Assert.IsFalse(0.IsIn(1, 2, 3));
+        }
+
+        [Test]
+        public void IsIn_ForStringInAcceptedValues_ShouldReturnTrue()
+        {
+            Assert.IsTrue("Hello".IsIn("Hi", "Hello"));
+        }
+
+        [Test]
+        public void IsIn_ForStringNotInAcceptedValues_ShouldReturnFalse()
+        {
+            Assert.IsFalse("Hey".IsIn("Hi", "Hello"));
+        }
+
+        [Test]
+        public void IsIn_ForNullValue_ShouldReturnFalse()
+        {
+            Assert.IsFalse((null as string).IsIn("Hello"));
+        }
+
+        [Test]
+        public void IsIn_ForNullValueAndNullInAcceptedValues_ShouldReturnTrue()
+        {
+            Assert.IsTrue((null as string).IsIn(null, "Hello"));
+        }
+
+        [Test]
+        public void IsIn_ForEnumInAcceptedValues_ShouldReturnTrue()
+        {
+            Assert.IsTrue(DayOfWeek.Saturday.IsIn(DayOfWeek.Saturday, DayOfWeek.Sunday));
+        }
+
+        [TestCase("hello", "Hello", true)]
+        [TestCase("HELLO", "Hello", true)]
+        [TestCase("hElLo", "Hello", true)]
+        [TestCase(" Hello", "Hello", false)]
+        [TestCase("Hello ", "Hello", false)]
+        [TestCase("Hello\r", "Hello", false)]
+        [TestCase(null, null, true)]
+        public void IsInIgnoreCase_ForGivenValueAndAcceptedValue_ShouldReturnExpected(string value, string acceptedValue, bool expected)
+        {
+            Assert.AreEqual(expected, value.IsInIgnoreCase(acceptedValue));
+        }
+
+        [Test]
+        public void IsInIgnoreCase_ForEmptyStringAndEmptyAcceptedValues_ShouldReturnFalse()
+        {
+            Assert.IsFalse(string.Empty.IsInIgnoreCase());
+        }
+
+        [Test]
+        public void IsInIgnoreCase_ForSameValuesInEnglishCulture_ShouldReturnTrue()
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            Assert.IsTrue("I".IsInIgnoreCase("i"));
+        }
+
+        [Test]
+        public void IsInIgnoreCase_ForDifferentValuesInTurkishCulture_ShouldReturnFalse()
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("tr-TR");
+            Assert.IsFalse("I".IsInIgnoreCase("i"));
+        }
+
+        [TestCase("hello", "Hello", true)]
+        [TestCase("hello\r", "Hello", true)]
+        [TestCase("\r\r\r\rhello\r", "Hello", true)]
+        [TestCase("hello ", "Hello", true)]
+        [TestCase(" HELLO", "Hello", true)]
+        [TestCase("hElLo ", "Hello", true)]
+        [TestCase("hEl Lo", "Hello", false)]
+        [TestCase(" ", "", true)]
+        [TestCase(" ", "\t", true)]
+        [TestCase(null, "\t", false)]
+        [TestCase(null, null, true)]
+        public void IsInIgnoreCaseWhiteSpace_ForGivenValueAndAcceptedValue_ShouldReturnExpected(string value, string acceptedValue, bool expected)
+        {
+            Assert.AreEqual(expected, value.IsInIgnoreCaseWhiteSpace(acceptedValue));
+        }
+    }
+}
---
+++ decomplex/ComparisonExtensions.cs
@@ -0,0 +1,55 @@
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+
+namespace decomplex
+{
+    /// <summary>
+    /// Extension methods to simplify comparisons.
+    /// </summary>
+    public static class ComparisonExtensions
+    {
+        /// <summary>
+        /// Checks if the value is among the accepted values.
+        /// </summary>
+        /// <typeparam name="T">Type of the compared values.</typeparam>
+        /// <param name="value">Value.</param>
+        /// <param name="acceptedValues">Accepted values.</param>
+        /// <returns>True if the value is among the accepted values, otherwise False.</returns>
+        public static bool IsIn<T>(this T value, params T[] acceptedValues)
+        {
+            return acceptedValues.Contains(value);
+        }
+
+        /// <summary>
+        /// Checks if the string value is among the accepted strings values. Character cases are ignored. 
+        /// </summary>
+        /// <param name="string">String.</param>
+        /// <param name="acceptedStrings">Accepted strings.</param>
+        /// <returns>True if the string is among the accepted strings, otherwise False.</returns>
+        public static bool IsInIgnoreCase(this string @string, params string[] acceptedStrings)
+        {
+            var compareInfo = Thread.CurrentThread.CurrentCulture.CompareInfo;
+            return acceptedStrings.Any(acceptedValue => 
+                @string == null && acceptedValue == null ||
+                @string != null && acceptedValue != null && compareInfo.IndexOf(acceptedValue, @string, CompareOptions.IgnoreCase) >= 0
+                );
+        }
+
+        /// <summary>
+        /// Checks if the string value is among the accepted strings. Character cases and white-space characters are ignored.
+        /// </summary>
+        /// <param name="string">String.</param>
+        /// <param name="acceptedStrings">Accepted strings.</param>
+        /// <returns>True if the string is among the accepted strings, otherwise False.</returns>
+        public static bool IsInIgnoreCaseWhiteSpace(this string @string, params string[] acceptedStrings)
+        {
+            var compareInfo = Thread.CurrentThread.CurrentCulture.CompareInfo;
+
+            return acceptedStrings.Any(acceptedValue => 
+                @string == null && acceptedValue == null ||
+                @string != null && acceptedValue != null && compareInfo.IndexOf(acceptedValue.Trim(), @string.Trim(), CompareOptions.IgnoreCase) >= 0
+                );
+        }
+    }
+}
